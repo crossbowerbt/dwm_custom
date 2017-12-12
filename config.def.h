192d6c3289cd6c620fc6a7caa6098c4606a6afe5
Emanuele Acri <emanuele.acri@ts-way.com>
2017-12-12 08:53:08 +0100

    config.def.h: set default tag for common applications
    Signed-off-by: Emanuele Acri <emanuele.acri@ts-way.com>

--- config.def.h
+++ config.def.h
@@ -27,8 +27,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",   NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Emacs",     NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Evolution", NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Pidgin",    NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
